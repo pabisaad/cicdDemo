5b84ef88d7034f23542fee5ea56549656cfeaff1
pabisaad <p.abisaad@pixel.com.lb>
2020-12-08 14:19:01 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
- import { HttpClient } from '@angular/common/http';
- @Component({
-   selector: 'app-root',
-   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
- })
- export class AppComponent {
-   book: any = { };
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  book: any = {};
 
-   constructor(http: HttpClient) {
-     http.get('https://api.angular.schule/book/9783864906466')
-       .subscribe(b => this.book = b);
-   }
- }
+  constructor(http: HttpClient) {
+    http.get('https://api.angular.schule/book/9783864906466')
+      .subscribe(b => this.book = b);
+  }
+}
